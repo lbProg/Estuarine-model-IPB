99aed4f6de093de6cca0343a3ebc50088e0daf51
Lucien Bastin <lucien.bastin@protonmail.com>
2025-09-25 15:29:17 +0200

    Moved variables into dictionary and pass this dict to equations

--- estuarine_model.py
+++ estuarine_model.py
@@ -9,7 +9,7 @@ from tools import equations as eq
 
 t_0 = 0 # Initial time
 t_f = 2 # Max time (d)
-dt = 1 / 24 # Time step (d), one time-step = one hour
+dt = 1 / 12 # Time step (d), one time-step = 24/x hour
 
 time = np.arange(t_0, t_f + dt, dt)
 
@@ -22,23 +22,26 @@ col_dims = int(width / res)
 
 # Define model variables
 
-light = classes.Variable2d(time, row_dims, col_dims, 0, "Light")
+variables = {
+  "light": classes.Variable2d(time, row_dims, col_dims, 0, "Light")
+}
 
 # Function to update model variables
 
-def compute_variables(t, row, col):
+def compute_variables(t, row, col, dt, variables):
   if (row_dims - row > bathymetry.bathymetry(col)) :
-    light.value[t, row, col] = eq.light(t, row * res, col * res)
+    variables['light'].value[t, row, col] = eq.light(t, row * res, col * res, dt, variables)
   else :
-    light.value[t, row, col] = np.nan # No value underground
+    # No value underground
+    variables['light'].value[t, row, col] = np.nan
 
 # Model loop
 
 for t in range(0, len(time)) :
   for row in range(0, row_dims) :
     for col in range(0, col_dims) :
-      compute_variables(t, row, col)
+      compute_variables(t, row, col, dt, variables)
 
 # Plor results in cross-section plot
 
-visu.cross_plot(light, t_0, t_f, dt, row_dims, col_dims, res)
+visu.cross_plot(variables['light'], t_0, t_f, dt, row_dims, col_dims, res)
