103292e8ae2a1a3e5fff7d08a4ccd1d81987b53b
Lucien Bastin <lucien.bastin@protonmail.com>
2025-09-25 17:10:45 +0200

    Added equations for nutrients and phytoplankton

--- tools/equations.py
+++ tools/equations.py
@@ -2,8 +2,24 @@ import math
 
 # Light availability
 
-def light(t, row, col, dt, variables):
+def light(t, row, col, res, dt, vars):
   periodicity = (math.sin(2 * math.pi * t * dt) + 1) # 24-hour loop
-  exponential_decay = 10 * math.exp(-0.3 * row)
+  exponential_decay = 10 * math.exp(-0.3 * row * res)
 
   return periodicity * exponential_decay
+
+def nutrients(t, row, col, res, dt, vars):
+  previous_val = vars["nutrients"].value[t - 1, row, col]
+
+  d = vars["tau"] * (vars["N0"] - previous_val)
+
+  return previous_val + d * dt
+
+def phytoplankton(t, row, col, res, dt, vars):
+  previous_val = vars["phytoplankton"].value[t - 1, row, col]
+  grazing = 0.1 * vars["light"].value[t - 1, row, col] * vars["nutrients"].value[t - 1, row, col]
+
+  d = grazing - vars["tau"] * previous_val
+
+  return previous_val + d * dt
+
