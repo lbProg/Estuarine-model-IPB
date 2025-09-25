e2c527ab22d6b561bc3561a9b378c64242a0a497
Lucien Bastin <lucien.bastin@protonmail.com>
2025-09-25 13:03:49 +0200

    Moved interactive plot to separate file

---
+++ tools/visualization.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+# Cross-section plot to show x, y and time
+
+def cross_plot(var, t_0, t_f, dt):
+  max = np.nanmax(var.value)
+  min = np.nanmin(var.value)
+
+  # Create the figure
+  fig, ax = plt.subplots()
+
+  cmap = plt.cm.viridis
+  cmap.set_bad('black')
+
+  tile = plt.imshow(var.value[0, :, :], cmap = cmap, vmin = min, vmax = max)
+
+  cbar = plt.colorbar(tile)
+  cbar.ax.set_ylabel(var.name)
+
+  ax.set_ylabel('Depth (m)')
+
+  # adjust the main plot to make room for the slider
+  fig.subplots_adjust(left = 0.25, bottom = 0.25)
+
+  # Make a horizontal slider to control the time
+  axtime = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+  time_slider = Slider(
+      ax = axtime,
+      label = 'Time (d)',
+      valmin = t_0,
+      valmax = t_f,
+      valinit = t_0,
+      valstep = dt
+  )
+
+  # The function to be called anytime a slider's value changes
+  def update(t):
+      t = int(t / dt)
+      ax.imshow(var.value[t, :, :], cmap = cmap, vmin = min, vmax = max)
+      fig.canvas.draw()
+
+  # register the update function
+  time_slider.on_changed(update)
+
+  plt.show()
