dd508f11290ffe08bd63fddedab3a21452abb1a9
Lucien Bastin <lucien.bastin@protonmail.com>
2025-09-25 17:10:23 +0200

    Added variables and tried plotting total phytoplankton by time

--- estuarine_model.py
+++ estuarine_model.py
@@ -11,7 +11,7 @@ t_0 = 0 # Initial time
 t_f = 2 # Max time (d)
 dt = 1 / 12 # Time step (d), one time-step = 24/x hour
 
-time = np.arange(t_0, t_f + dt, dt)
+time = np.arange(t_0, t_f + dt/2, dt)
 
 depth = 20
 width = 20
@@ -23,25 +23,47 @@ col_dims = int(width / res)
 # Define model variables
 
 variables = {
-  "light": classes.Variable2d(time, row_dims, col_dims, 0, "Light")
+  "tau": 0.1, # turnover rate
+  "N0": 1, # Nutrient input
+  "light": classes.Variable2d(time, row_dims, col_dims, 0, "Light"),
+  "nutrients": classes.Variable2d(time, row_dims, col_dims, 0.1, "Nutrients"),
+  "phytoplankton": classes.Variable2d(time, row_dims, col_dims, 0.1, "Phytoplankton")
 }
 
 # Function to update model variables
 
-def compute_variables(t, row, col, dt, variables):
+def compute_variables(t, row, col, res, dt, variables):
   if (row_dims - row > bathymetry.bathymetry(col)) :
-    variables['light'].value[t, row, col] = eq.light(t, row * res, col * res, dt, variables)
+    variables['light'].value[t, row, col] = eq.light(t, row, col, res, dt, variables)
+    variables['nutrients'].value[t, row, col] = eq.nutrients(t, row, col, res, dt, variables)
+    variables['phytoplankton'].value[t, row, col] = eq.nutrients(t, row, col, res, dt, variables)
   else :
     # No value underground
     variables['light'].value[t, row, col] = np.nan
+    variables['nutrients'].value[t, row, col] = np.nan
+    variables['phytoplankton'].value[t, row, col] = np.nan
 
 # Model loop
 
-for t in range(0, len(time)) :
+for t in range(1, len(time)) :
   for row in range(0, row_dims) :
     for col in range(0, col_dims) :
-      compute_variables(t, row, col, dt, variables)
+      compute_variables(t, row, col, res, t, variables)
 
 # Plor results in cross-section plot
 
-visu.cross_plot(variables['light'], t_0, t_f, dt, row_dims, col_dims, res)
+# visu.cross_plot(variables['phytoplankton'], t_0, t_f, dt, row_dims, col_dims, res)
+
+sum_by_time = variables['phytoplankton'].value[:, :, :].sum(axis = 1).sum(axis = 1).tolist()
+
+visu.line_plot(sum_by_time, time)
+
+# import matplotlib.pyplot as plt
+
+# plt.close()
+
+# ax = plt.subplot()
+
+# ax.plot(time, sum_by_time, "r--", label = "var.name")
+
+# plt.show()
