5240998525d865aa69dd80a07b83c29a0178a91b
Lucien Bastin <lucien.bastin@protonmail.com>
2025-09-25 15:29:38 +0200

    Take into account model state with all variables

--- tools/equations.py
+++ tools/equations.py
@@ -2,8 +2,8 @@ import math
 
 # Light availability
 
-def light(t, row, col):
-  periodicity = (math.sin(2 * math.pi / 24 * t) + 1)
-  exponential_decay = 10 * math.exp(-0.6 * row)
-  
+def light(t, row, col, dt, variables):
+  periodicity = (math.sin(2 * math.pi * t * dt) + 1) # 24-hour loop
+  exponential_decay = 10 * math.exp(-0.3 * row)
+
   return periodicity * exponential_decay
